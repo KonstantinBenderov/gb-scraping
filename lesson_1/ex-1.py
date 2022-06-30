7d535d3faa3e8f79a147b8c0a9f52844ed5cf5dd
Konstantin <57568587+KonstantinBenderov@users.noreply.github.com>
2022-06-30 21:14:24 +0300

    Ex. #1

---
+++ lesson_1/ex-1.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+from pprint import pprint
+import requests
+
+# set username
+user = 'pixijs'
+# ask for an username if it is not set
+if not user:
+    user = input('Enter username: ')
+
+# infinite loop while username is invalid
+while True:
+    url = f'https://api.github.com/users/{user}'
+    try:
+        response = requests.get(url)
+        if response.ok:
+            j_response = response.json()
+            repos_url = j_response['repos_url']
+            break
+        else:
+            print('Too many requests. Try again later.')
+            exit(1)
+    except Exception:
+        user = input('Wrong username! Try again!\nEnter username: ')
+
+repos_response = dict()
+repos_response[user] = requests.get(repos_url).json()
+
+# save repos_response in *.json file
+file = 'repos.json'
+with open(file, 'w', encoding='utf-8') as out:
+    json.dump(repos_response, out, indent=4, ensure_ascii=False)
+
+repos = {user: []}
+repos[user].append(
+    {repo['name']: repo['svn_url'] for repo in repos_response[user]}
+)
+
+pprint(repos)
